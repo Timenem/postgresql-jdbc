e81ec62f18c32171fe95792e0db7eba2fb88cc9b
Timenem <56394536+Timenem@users.noreply.github.com>
2022-07-10 10:24:38 +0000

    Update trafic.sql

--- trafic.sql
+++ trafic.sql
@@ -45,3 +45,40 @@ SET sum_fine=(SELECT sum_fine FROM traffic_violation WHERE traffic_violation.vio
 WHERE sum_fine isNULL;
 
 SELECT * from fine;
+
+
+
+
+
+#Вывести фамилию, номер машины и нарушение только для тех водителей, 
+#которые на одной машине нарушили одно и то же правило   два и более раз. 
+#При этом учитывать все нарушения, независимо от того оплачены они или нет. 
+#Информацию отсортировать в алфавитном порядке, сначала по фамилии водителя, потом по номеру машины и, наконец, по нарушению.
+SELECT name, number_plate, violation
+FROM fine
+GROUP BY name, number_plate, violation
+HAVING  COUNT(*) >1  ORDER BY name, number_plate, violation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
