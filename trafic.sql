2d9a2ce4d5e500f92b746e33772b95b3fbf446ae
Timenem <56394536+Timenem@users.noreply.github.com>
2022-07-12 17:26:03 +0000

    Update trafic.sql

--- trafic.sql
+++ trafic.sql
@@ -65,6 +65,11 @@ HAVING  COUNT(*) >1  ORDER BY name, number_plate, violation;
 DELETE  from fine WHERE date_violation < '2020-02-01';
 
 
+#Создать новую таблицу back_payment, куда внести информацию о неоплаченных штрафах 
+#(Фамилию и инициалы водителя, номер машины, нарушение, сумму штрафа  и  дату нарушения) из таблицы fine.
+CREATE TABLE back_payment AS  
+SELECT name, number_plate, violation, sum_fine, date_violation FROM fine WHERE date_payment ISNULL;
+
 
 
 
