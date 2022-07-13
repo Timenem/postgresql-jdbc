4a200b11bb053219293a75e567708a18b07467ed
Timenem <56394536+Timenem@users.noreply.github.com>
2022-07-13 14:46:16 +0000

    Create books.sql

---
+++ books.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE author (
+    author_id INTEGER PRIMARY KEY UNIQUE,
+    name_author VARCHAR(50)
+    );
+CREATE TABLE genre (
+    genre_id INTEGER PRIMARY KEY,
+    name_genre VARCHAR(30)
+    );
+CREATE TABLE book (
+    book_id INTEGER PRIMARY KEY , 
+    title VARCHAR(50), 
+    author_id INT NOT NULL, 
+    genre_id INT, 
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id) REFERENCES author (author_id),
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
+);
+
+/*
+insert INTO author (name_author) values
+    ('Булгаков М.А.'),
+    ('Достоевский Ф.М.'),
+    ('Есенин С.А.'),
+    ('Пастернак Б.Л.');
+*/
+  
+  
